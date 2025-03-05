d5b060bfd8b750c26d02a3818e5192ee71a02aaf
Unichtogiteli3000 <flowersoftherussia@gmail.com>
2025-03-05 11:57:47 +0300

    Add files via upload

---
+++ 2dz/pointer.cpp
@@ -0,0 +1,41 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+/*void Swap(int* pa, int* pb) {
+    int c = *pb;
+    *pb = *pa;
+    *pa = c;
+}*/
+
+void Swap(int &a, int &b) {
+    int c = a;
+    a = b;
+    b = c;
+}
+
+int main()
+{/*
+    int x = 3;
+    int* p = &x; // &x - адрес переменной x в памяти, p - указатель, который хрнаит адрес x
+    cout << x << ' ' << *p << ' ' << p << endl; // * - переходим по адресу в памяти для получения значения
+    p++;
+    cout << p << endl;
+    cout << p - &x << endl;
+
+    typedef unsigned char byte; // Переименовываем uc в byte
+
+    byte *pb = (byte *)--p; // вернули указатель на x и преобразовали
+    for (byte *pt = pb; pt - pb < sizeof(int); pt++) // Цикл выполняется, пока разница между pt и pb меньше размера int (обычно 4 байта).
+        cout << (int)*pt << ' '; // выводим как int
+    cout << endl;*/
+
+    int a = 3, b = 5;
+    cout << a << ' ' << b << endl;
+    /*int c = b;
+    b = a;
+    a = c;*/
+    Swap(a, b);
+    cout << a << ' ' << b << endl;
+}
---
+++ 2dz/strings.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int len(const char* s) {
+    int k = 0;
+    while (*s != '\0') {
+        s++;
+        k++;
+    }
+    return k;
+}
+
+int compare(const char* s, const char* t) {
+    if (len(s) > len(t)) {
+        return 1;
+    }
+    else if (len(s) < len(t)) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int main()
+{
+    char str[] = "Hello!";
+    //cout << str << endl;
+    //cout << strlen(str) << ' ' << sizeof(str) << endl; // sizeof подсчитывает значение, включаю завершающий нулевой символ '\0'
+
+    //char t[32];
+
+    //for (char* pd = t, *ps = str; *pd++ = *ps++;);
+    //cout << t  << ' ' << strlen(t) << ' ' << sizeof(t) << endl;
+    char v[32];
+    /*for (int i = 0; i < strlen(str); i++) {
+        cout << str[i] << endl;
+        v[i] = str[i];
+    }*/
+    char* pd = v, * ps = str;
+    for (int i = 0; i < strlen(str); i++, ps++, pd++) {
+        *pd = *ps;
+    }
+    v[strlen(str)] = '\0';
+    cout << v << endl;
+
+    cout << len(str) << endl;
+}
